d969e893ebc1d6aed4c6345ae597eaac179053e1
JacobLandgren <jlandgre@ucsd.edu>
2022-05-23 23:38:22 -0700

    Hw7

--- server/src/server.py
+++ server/src/server.py
@@ -21,6 +21,9 @@ def get_IA(req):
 def get_features(req):
   return FileResponse("features.html")
 
+def get_revenue(req):
+  return FileResponse("revenue.html")
+
 ''' Route Configurations '''
 if __name__ == '__main__':
   config = Configurator()
@@ -39,6 +42,9 @@ if __name__ == '__main__':
   
   config.add_route('get_IA', '/IA')
   config.add_view(get_IA, route_name='get_IA')
+  
+  config.add_route('get_revenue', '/IA')
+  config.add_view(get_revenue, route_name='get_revenue')
   
   config.add_route('get_features', '/features')
   config.add_view(get_features, route_name='get_features')
